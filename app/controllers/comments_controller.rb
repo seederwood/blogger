cd55d7b22d7a597ec28f17c36b0158054c712c37
Ray Climie <seederwood@gmail.com>
2017-05-31 16:41:09 -0300

    blogger project, I2 adding comments incl. edit & delete (extras)

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,44 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+
+    @comment.save
+
+    flash.notice = 'Comment Created!'
+
+    redirect_to article_path(@comment.article)
+  end
+
+  def edit
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.article_id = params[:article_id]
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.article_id = params[:article_id]
+    @comment.update(comment_params)
+
+    flash.notice = 'Comment Updated!'
+
+    redirect_to article_path(@comment.article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+
+    @comment.destroy
+
+    flash.notice = 'Comment Deleted!'
+
+    redirect_to article_path(@article)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:author_name, :body)
+  end
+end
